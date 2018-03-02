b535c82eef5f3648478fb6420113344a2e8d6bd4
aashishshrestha5532 <aashishshrestha5532@gmail.com>
2018-03-02 21:13:37 +0545

    webview started

---
+++ app/src/main/java/com/alchemist/medilac/Medilac.java
@@ -0,0 +1,31 @@
+package com.alchemist.medilac;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.KeyEvent;
+import android.webkit.WebView;
+
+public class Medilac extends AppCompatActivity {
+    WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_medilac);
+
+         webView=(WebView) findViewById(R.id.webview);
+        webView.loadUrl("file:///android_asset/sample.html");
+        webView.getSettings().setJavaScriptEnabled(true);
+
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if ((keyCode == KeyEvent.KEYCODE_BACK) && webView.canGoBack()) {
+            webView.goBack();
+            return true;
+        }
+        return super.onKeyDown(keyCode, event);
+    }
+}
+
